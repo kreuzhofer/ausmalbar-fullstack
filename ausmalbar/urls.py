892ff0155960775f6b99ab80bc7b149ac987a972
Daniel Kreuzhofer <1763467+kreuzhofer@users.noreply.github.com>
2025-06-02 09:09:19 +0200

    fix ads.txt serving

--- ausmalbar/urls.py
+++ ausmalbar/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls import handler404, handler500
 from django.views.generic import TemplateView
 from django.contrib.sitemaps.views import sitemap
 from django.conf.urls.i18n import i18n_patterns
+from django.views.static import serve
+import os
 
 from coloring_pages.sitemaps import sitemaps
 
@@ -23,10 +25,10 @@ urlpatterns += [
         template_name='robots.txt', 
         content_type='text/plain'
     )),
-    path('ads.txt', TemplateView.as_view(
-        template_name='ads.txt',
-        content_type='text/plain'
-    )),
+    path('ads.txt', serve, {
+        'document_root': os.path.join(settings.BASE_DIR, 'static'),
+        'path': 'ads.txt',
+    }, name='ads_txt'),
     # Sitemap
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     # Language switcher
